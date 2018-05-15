c1d4c8fc76e4bad5ff51a21e8d778227057fb83b
alenadmit <dmitalyona@yandex.ru>
2018-05-16 00:44:47 +0300

    added start cards

--- src/Start/Start.jsx
+++ src/Start/Start.jsx
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from 'react';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
+import yandexMapsLogo from './yandex.jpeg';
+import googleMapsLogo from './google.png';
+import gisMapsLogo from './2gis.jpg';
 import startStyles from './Start.css';
 import { withStyles } from 'material-ui/styles';
 
@@ -11,6 +15,13 @@ const styles = theme => ({
         paddingBottom: 16,
         marginTop: theme.spacing.unit * 3,
     }),
+    card: {
+        maxWidth: 300,
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%',
+    },
 });
 
 class Start extends Component {
@@ -25,6 +36,68 @@ class Start extends Component {
                 <Paper
                     className={classes.root}
                     elevation={4}>
+                    <div className="containerMaps">
+                        <Card className={classes.card}>
+                            <CardMedia
+                                className={classes.media}
+                                image={yandexMapsLogo}
+                                title="Яндекс.Карты"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="headline" component="h2">
+                                    Яндекс.Карты
+                                </Typography>
+                                <Typography component="p">
+                                    Один из самых популярных поисково-информационных картографических сервисов, используемых в России
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Узнать больше...
+                                </Button>
+                            </CardActions>
+                        </Card>
+                        <Card className={classes.card}>
+                            <CardMedia
+                                className={classes.media}
+                                image={googleMapsLogo}
+                                title="Google Maps"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="headline" component="h2">
+                                    Google Maps
+                                </Typography>
+                                <Typography component="p">
+                                    Глобальный картографический сервис, использующийся по всему миру
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Узнать больше...
+                                </Button>
+                            </CardActions>
+                        </Card>
+                        <Card className={classes.card}>
+                            <CardMedia
+                                className={classes.media}
+                                image={gisMapsLogo}
+                                title="2ГИС"
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="headline" component="h2">
+                                    2ГИС
+                                </Typography>
+                                <Typography component="p">
+                                    Картографический сервис, предоставляющий информацонно-поисковый справочник городов
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Узнать больше...
+                                </Button>
+                            </CardActions>
+                        </Card>
+                    </div>
                     <Typography
                         variant="headline"
                         component="h3">
