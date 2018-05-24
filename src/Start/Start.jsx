aa3ae1f98e2357ceb4502917e403a9739f38b6f2
Alena Dmitrievskaya <Alena_Dmitrievskaya@epam.com>
2018-05-24 16:26:36 +0300

    added methot map for cards

--- src/Start/Start.jsx
+++ src/Start/Start.jsx
@@ -27,18 +27,18 @@ const styles = theme => ({
 const Maps = [
     {
         name: 'Яндекс.Карты',
-        title: 'Яндекс.Карты',
-        desc: 'Один из самых популярных поисково-информационных картографических сервисов, используемых в России'
+        desc: 'Один из самых популярных поисково-информационных картографических сервисов, используемых в России',
+        img: yandexMapsLogo
     },
     {
         name: 'Google Maps',
-        title: 'Google Maps',
-        desc: 'Глобальный картографический сервис, использующийся по всему миру'
+        desc: 'Глобальный картографический сервис, использующийся по всему миру',
+        img: googleMapsLogo
     },
     {
         name: '2ГИС',
-        title: '2ГИС',
-        desc: 'Картографический сервис, предоставляющий информацонно-поисковый справочник городов'
+        desc: 'Картографический сервис, предоставляющий информацонно-поисковый справочник городов',
+        img: gisMapsLogo
     }
 ]
 
@@ -50,79 +50,34 @@ class Start extends Component {
     render() {
         const { classes } = this.props;
         const maps = Maps.map(maps => (
-            <Card
-              key={maps.name}
-              title={maps.title}
-              desc={maps.desc}
-            />)
-          );
-        const mapsAPI = this
+            <Card className={classes.card}>
+                <CardMedia
+                    className={classes.media}
+                    image={maps.img}
+                    title={maps.name}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="headline" component="h2">
+                        {maps.name}
+                    </Typography>
+                    <Typography component="p">
+                        {maps.desc}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Узнать больше...
+                    </Button>
+                </CardActions>
+            </Card>)
+        );
         return (
             <Fragment>
                 <Paper
                     className={classes.root}
                     elevation={4}>
                     <div className="containerMaps">
-                        <Card className={classes.card}>
-                            <CardMedia
-                                className={classes.media}
-                                image={yandexMapsLogo}
-                                title={maps.name}
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="headline" component="h2">
-                                    {maps.name}
-                                </Typography>
-                                <Typography component="p">
-                                    Один из самых популярных поисково-информационных картографических сервисов, используемых в России
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Узнать больше...
-                                </Button>
-                            </CardActions>
-                        </Card>
-                        <Card className={classes.card}>
-                            <CardMedia
-                                className={classes.media}
-                                image={googleMapsLogo}
-                                title="Google Maps"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="headline" component="h2">
-                                    Google Maps
-                                </Typography>
-                                <Typography component="p">
-                                    Глобальный картографический сервис, использующийся по всему миру
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Узнать больше...
-                                </Button>
-                            </CardActions>
-                        </Card>
-                        <Card className={classes.card}>
-                            <CardMedia
-                                className={classes.media}
-                                image={gisMapsLogo}
-                                title="2ГИС"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="headline" component="h2">
-                                    2ГИС
-                                </Typography>
-                                <Typography component="p">
-                                    Картографический сервис, предоставляющий информацонно-поисковый справочник городов
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size="small" color="primary">
-                                    Узнать больше...
-                                </Button>
-                            </CardActions>
-                        </Card>
+                        {maps}
                     </div>
                     <Typography
                         variant="headline"
